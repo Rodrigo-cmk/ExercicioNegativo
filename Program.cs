78c6e92ba7615e87f4e8998714004871dd86dcd9
= <=>
2021-05-06 18:42:59 -0300

    Exercício negativo com sub-rotina

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ExercicioNegativo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            digite("Digite um número natural: ");
+            valor(Console.ReadLine());
+        }
+
+        static void digite(string digite)
+        {
+            Console.Write(digite);
+        }
+
+        static void valor(string valor)
+        {
+            int a;
+            a = Convert.ToInt32(valor);
+
+            if (a < 0)
+            {
+                Console.WriteLine("Você digitou um número negativo.");
+            }
+
+            else 
+            {
+                Console.WriteLine($"Você digitou {a}.");
+            }
+        }
+    }
+}
